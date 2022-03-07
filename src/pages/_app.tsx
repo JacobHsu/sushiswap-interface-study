95cc34c503015b755f3731648994e424790c3858
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-07 11:45:40 +0800

    fix: _app remove AppProps by  @ts-ignore TYPE NEEDS FIXING

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+// @ts-ignore TYPE NEEDS FIXING
+function MyApp({ Component, pageProps }) {
+
+  // Allows for conditionally setting a layout to be hoisted per page
+  const Layout = Component.Layout
+
   return <Component {...pageProps} />
 }
 
