d4989eb9d35f1a0653816eacc1ae3ecdb74785ce
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-08 11:22:13 +0800

    feat: Header next.config tailwindcss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const { screens } = defaultTheme
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    breakpoints: screens,
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,26 @@
+import React, { Fragment, useEffect } from 'react'
 import '../styles/globals.css'
 // import type { AppProps } from 'next/app'
+import DefaultLayout from 'app/layouts/Default'
 
 // @ts-ignore TYPE NEEDS FIXING
 function MyApp({ Component, pageProps }) {
 
   // Allows for conditionally setting a layout to be hoisted per page
-  const Layout = Component.Layout
+  const Layout = Component.Layout || DefaultLayout
 
-  return <Component {...pageProps} />
+  // Allows for conditionally setting a guard to be hoisted per page
+  const Guard = Component.Guard || Fragment
+
+  return (
+    <>
+      <Layout>
+        <Guard>
+          <Component {...pageProps} />
+        </Guard>
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
